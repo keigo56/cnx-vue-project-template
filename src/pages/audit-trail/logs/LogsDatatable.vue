f38c9b98ebac148e73217855a7f077ec5811f2c3
Keigo Fujita <keigovfujita@gmail.com>
2023-07-06 15:37:33 +0800

    Update Git Ignore
    - Updated Git Ignore to add logs directory

---
+++ src/pages/audit-trail/logs/LogsDatatable.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1 class="mb-5 text-2xl font-semibold dark:text-white">Logs</h1>
+
+  <DataTable
+    source-url="/api/datatable/audit-trail/logs"
+    export-url="/api/datatable/audit-trail/logs/export"
+    export-file-name="logs.csv"
+    :with-action-items="false"
+    ref="logsDatatable"
+  >
+  </DataTable>
+</template>
+
+<script setup>
+import DataTable from "@/modules/datatable/DataTable.vue";
+import { PlusIcon } from "@heroicons/vue/24/outline/index.js";
+import { onMounted, ref } from "vue";
+
+const logsDatatable = ref(null);
+
+onMounted(() => {
+  logsDatatable.value.setDefaultSort({
+    key: "activity_log_created_at",
+    direction: "desc",
+  });
+});
+</script>
---
+++ src/pages/audit-trail/logs/LogsIndexPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <BaseLayout>
+    <div class="p-5">
+      <LogsDatatable />
+    </div>
+  </BaseLayout>
+</template>
+
+<script setup>
+import BaseLayout from "@/layouts/BaseLayout.vue";
+import LogsDatatable from "@/pages/audit-trail/logs/LogsDatatable.vue";
+</script>
