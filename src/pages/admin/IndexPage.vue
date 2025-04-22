1e4d6e2a522fa8bf5a646fbf42982bc4d4da0f30
Keigo Victor Fujita <keigovfujita@gmail.com>
2025-04-22 21:16:51 +0800

    refactor(admin-index-page): enhance layout with Card components and update styles

--- src/pages/admin/IndexPage.vue
+++ src/pages/admin/IndexPage.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="p-5">
-    <h1>Homepage</h1>
+  <div class="p-5 h-full">
+    <h1 class="mb-5 text-2xl font-semibold dark:text-white">Homepage</h1>
+    <div class="grid grid-cols-3 gap-5 mb-5">
+      <Card class="h-52 bg-secondary/40 dark:border-none"> </Card>
+      <Card class="h-52 bg-secondary/40 dark:border-none"> </Card>
+      <Card class="h-52 bg-secondary/40 dark:border-none"> </Card>
+    </div>
+    <Card class="h-100 bg-secondary/40 dark:border-none"> </Card>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import Card from "@/components/ui/card/Card.vue";
+</script>
